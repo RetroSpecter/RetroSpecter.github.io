e005d9eb10e75013318c71a013982f3a6cd99574
RetroSpecter <InsertNameHere4897@gmail.com>
2017-02-02 14:46:08 -0800

    Create frontPage.php

---
+++ frontPage.php
@@ -0,0 +1,16 @@
+<?php 
+	include("Common.php");
+	top();
+?>
+		<div id="content">
+			<h1>About</h1>
+			<p>
+				Jose Pacio is a student at the University of Washington in Seattle studying to become a programmer.
+				most of his skills are derived from video game programming but he also has some experience in web development as well.
+			</p>
+		</div>
+		<div id="footer">
+			Created by Jose Pacio 2017
+		</div>
+	</body>
+</html>
